cd03bbf09fd1a9af32db4dc444c529a5cc6d666d
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2022-04-16 12:36:48 +0200

    test

--- run-tools.sh
+++ run-tools.sh
@@ -20,10 +20,11 @@ echo "::group::Podman Hello"
 podman run quay.io/podman/hello
 echo "::endgroup::"
 
-echo "::group::Test lifecycle of a container with crun"
+echo "::group::Test lifecycle of a container with crun/runc"
 pushd ~/code/fwilhe-containers/container-image
 nc -lkU mySocket.sock &
-sudo DEBIAN_FRONTEND=noninteractive apt-get install python3-pip
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install python3-pip
+sudo pip --no-input install pytest
 pytest -svv
 popd
 echo "::endgroup::"
