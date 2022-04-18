ba8d5327f0d619b6427ca2fdbc22eea6009ec73f
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2022-04-18 18:58:29 +0200

    Run pytest tests as part of ci

---
+++ vm-resources/test-scripts/test_tools_installed.py
@@ -0,0 +1,28 @@
+import pytest
+import subprocess
+import json
+
+def run_command(command):
+    print(command)
+    output = subprocess.run(command, capture_output=True)
+    stdout = output.stdout.decode("utf-8").rstrip()
+    stderr = output.stderr.decode("utf-8").rstrip()
+    return (stdout, stderr)
+
+@pytest.mark.parametrize("command,expected", [
+    ('bwrap --version', 0),
+    ('conmon --version', 0),
+    ('containerd --version', 0),
+    ('crio --version', 0),
+    ('crun --version', 0),
+    ('podman --version', 0),
+    ('podman info', 0),
+    ('rootlesskit --version', 0),
+    ('runc --version', 0),
+    ('runsc --version', 0),
+    ('skopeo --version', 0),
+])
+def test_crun_container_lifecycle(command, expected):
+    stdout, stderr = run_command(command.split(' '))
+    assert stdout != ""
+    assert stderr == ""
